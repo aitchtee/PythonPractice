193a804fa42ed884a831aedebf39aac091d34c10
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-02-16 19:11:43 +0300

    done first ind (not optimize)

--- individuals/first/firstInd.py
+++ individuals/first/firstInd.py
@@ -5,6 +5,16 @@
 последовательности не превышает 10000.
 '''
 f = open('first.txt')
-for num in f:
-    print(num)
+kol = f.read(1)
+arr = list(map(int, f.read().split()))
+sortArr = sorted(arr)
+minSum = sortArr[len(arr)-1] + sortArr[len(arr)-2]
+# print('Старый минимум', minSum)
 f.close()
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        if abs(i-j) >= 4:
+            newMin = arr[i] + arr[j]
+            if newMin < minSum:
+                minSum = newMin
+print('Новый минимум', minSum)
