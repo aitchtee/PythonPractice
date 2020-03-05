a02e0082b2fe235937f7e89aa827f003c049c989
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-03-05 17:23:03 +0300

    pretty write to json

--- individuals/db/db.py
+++ individuals/db/db.py
@@ -18,36 +18,41 @@ if create:
                    "crew_id INTEGER NOT NULL,"
                    "style_id INTEGER NOT NULL,"
                    "dancer_name TEXT UNIQUE NOT NULL,"
-                   "FOREIGN KEY(crew_id) REFERENCE crews(id),"
-                   "FOREIGN KEY(style_id) REFERENCE styles(id))")
+                   "FOREIGN KEY(crew_id) REFERENCES crews(id),"
+                   "FOREIGN KEY(style_id) REFERENCES styles(id))")
     db.commit()
 
 cursor = db.cursor()
+# cursor.execute("INSERT INTO crews(crew_name) VALUES ('No Team')")
+# cursor.execute("INSERT INTO crews(crew_name) VALUES ('Jabbawockeez')")
+# cursor.execute("INSERT INTO crews(crew_name) VALUES ('Kinjaz')")
+
 # cursor.execute("INSERT INTO styles(style_name) VALUES('Hip-Hop')")
 # cursor.execute("INSERT INTO styles(style_name) VALUES('Popping')")
 # cursor.execute("INSERT INTO styles(style_name) VALUES('Breaking')")
-cursor.execute("INSERT INTO dancers(crew_id, style_id, dancer_name) VALUES(2, 3, 'Steven Lor')")
+
+# cursor.execute("INSERT INTO dancers(crew_id, style_id, dancer_name) VALUES(2, 3, 'Steven Lor')")
+# cursor.execute("INSERT INTO dancers(crew_id, style_id, dancer_name) VALUES(1, 2, 'Dytto')")
+# cursor.execute("INSERT INTO dancers(crew_id, style_id, dancer_name) VALUES(1, 1, 'Taylor Hatala')")
+# cursor.execute("INSERT INTO dancers(crew_id, style_id, dancer_name) VALUES(2, 3, 'Ben Chung')")
+# cursor.execute("INSERT INTO dancers(crew_id, style_id, dancer_name) VALUES(2, 1, 'Phil Tayag')")
 db.commit()
 
 cursor2 = db.cursor()
-cursor2.execute("SELECT * FROM crews")
+cursor2.execute("SELECT d.id, c.crew_name, s.style_name, d.dancer_name FROM dancers d "
+                "INNER JOIN crews c ON d.crew_id = c.id "
+                "INNER JOIN styles s ON d.style_id = s.id")
+# cursor2.execute("select * from dancers ORDER BY id")
 
 records = cursor2.fetchall()
-dictRecord = dict(records)
+res = {}
+resList = []
 for i, record in enumerate(records):
-    print("{0} : {1}".format(record[0], record[1]))
+    res["id"] = records[i][0]
+    res["crew_name"] = records[i][1]
+    res["style_name"] = records[i][2]
+    res["dancer_name"] = records[i][3]
+    resList.append(res.copy())
 
 with open("data_file.json", "w") as write_file:
-    json.dump(dictRecord, write_file, indent=4)
-
-print("===============================")
-
-cursor3 = db.cursor()
-cursor3.execute("SELECT * FROM styles")
-
-records = cursor3.fetchall()
-dictRecord = dict(records)
-
-for i, record in enumerate(records):
-    print("{0} : {1}".format(record[0], record[1]))
-
+    json.dump(resList, write_file, indent=4)
